55b70c4af7a259cd3affab4a4f68b00337c84632
andreykoleber <andrei.koleber@gmail.com>
2018-02-25 23:58:00 +0600

    added action bar in xml

--- app/src/main/java/com/bigderanch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bigderanch/android/criminalintent/CrimeListFragment.java
@@ -10,6 +10,8 @@ import android.text.Layout;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -38,6 +40,12 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         updateUi();
         return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -86,6 +94,11 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
+    }
 
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
 
