0580294b8098de04b25efc30106db37d3a72a48c
andreykoleber <andrei.koleber@gmail.com>
2018-03-12 23:32:37 +0600

    Add two panel interface for phone and tablet

--- app/src/main/java/com/bigderanch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bigderanch/android/criminalintent/CrimeListActivity.java
@@ -2,9 +2,6 @@ package com.bigderanch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-/**
- * Created by andre on 06.02.2018.
- */
 
 public class CrimeListActivity extends SingleFragmentActivity {
     @Override
@@ -12,4 +9,8 @@ public class CrimeListActivity extends SingleFragmentActivity {
         return CrimeListFragment.newInstance();
     }
 
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_masterdetail;
+    }
 }
--- app/src/main/java/com/bigderanch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bigderanch/android/criminalintent/SingleFragmentActivity.java
@@ -1,13 +1,11 @@
 package com.bigderanch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by andre on 06.02.2018.
- */
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
@@ -16,7 +14,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
@@ -26,5 +24,10 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+    }
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
     }
 }
